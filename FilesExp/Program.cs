b8600b6e1e2183202ab9010e0ab73c3711ac097a
vamsi <bellamvamsikrishna3415@gmail.com>
2022-03-14 19:55:48 +0530

    Something with files

---
+++ FilesExp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName,"",SearchOption.AllDirectories);
+
+   /*  foreach(var fileName in foundFiles) {
+        if(fileName.EndsWith("sales.json")){
+            salesFiles.Add(fileName);
+        }
+    } */
+
+    foreach (var fileName in foundFiles)
+    {
+        var extension = Path.GetExtension(fileName);
+        if(extension == ".json") {
+            salesFiles.Add(fileName);
+        }
+    }
+
+    return salesFiles;
+}
+
+//var salesFiles = FindFiles("stores");
+var currentDirectrory = Directory.GetCurrentDirectory();
+var storesDirectroy = Path.Combine(currentDirectrory,"stores");
+
+var salesFiles = FindFiles(storesDirectroy);
+
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+var salesTotalDir = Path.Combine(currentDirectrory,"salesTotalDir");
+
+Directory.CreateDirectory(salesTotalDir);
+
+File.WriteAllText(Path.Combine(salesTotalDir,"total.txt"),String.Empty);
---
+++ FilesExp/README.md
@@ -0,0 +1,1 @@
+The stores folder data taken from [mslearn-dotnet-file repo from microsoft](https://github.com/MicrosoftDocs/mslearn-dotnet-files)
