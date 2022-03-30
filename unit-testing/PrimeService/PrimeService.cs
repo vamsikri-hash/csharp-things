8131d34ab91a34885ad1f8b978471e38e84984b1
vamsi <bellamvamsikrishna3415@gmail.com>
2022-03-30 09:56:18 +0530

    Add unit test ex

---
+++ unit-testing/PrimeService/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
